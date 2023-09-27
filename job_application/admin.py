61264668daf7ca147aadcb8b10ca2888ab65da3a
Ulises <uliseslv55@gmail.com>
2023-09-27 17:15:24 -0600

    Modified the admin interface by adding titles to the table, a search bar, and a filter by date and occupation

--- job_application/admin.py
+++ job_application/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Form
 
-admin.site.register(Form)
+
+class FormAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "date", "occupation")
+    search_fields = ("first_name", "last_name", "email")
+    list_filter = ("date", "occupation")
+    readonly_fields = ("occupation", )
+
+
+admin.site.register(Form, FormAdmin)
